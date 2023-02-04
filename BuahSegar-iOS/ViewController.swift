99c562cd886f618b0b351a84d1b8ff3784d5b9dd
gafilianog <gafiliano@gmail.com>
2023-02-04 11:48:06 +0700

    Initial Commit

---
+++ BuahSegar-iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BuahSegar-iOS
+//
+//  Created by prk on 04/02/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
